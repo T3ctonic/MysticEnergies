99b3b6e2efab41858f8dd8d7b5afd7274d19b5eb
T3ctonic <T3ctonicPlays@gmail.com>
2014-06-14 23:01:21 +0100

    Attempt at Adding Item

--- src/main/java/com/t3ctonic/mysicenergies/data/item/ItemDarkEnergy.java
+++ src/main/java/com/t3ctonic/mysicenergies/data/item/ItemDarkEnergy.java
@@ -3,16 +3,8 @@ package com.t3ctonic.mysicenergies.data.item;
 import net.minecraft.item.Item;
 public class ItemDarkEnergy extends Item {
 
-    public ItemDarkEnergy(int par1)
-    {
-        super(par1);
-        this.setCreativeTab(mysticenergies.TabMysticEnergies);
-    }
-
-    public void registerIcons(IconRegister iconRegister)
-    {
-        itemIcon = iconRegister.registerIcon("MysticEnergies:DarkEnergy");
-    }
+    public ItemDarkEnergy(int par1){
 
     }
 }
+
