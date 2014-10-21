4dcd83a7811fc69dedae7d6bdc43ecbc01afc2ae
T3ctonic <T3ctonicPlays@gmail.com>
2014-10-21 16:23:20 +0100

    Added more tools

--- src/main/java/com/t3ctonic/mysticenergies/items/ModItems.java
+++ src/main/java/com/t3ctonic/mysticenergies/items/ModItems.java
@@ -7,15 +7,24 @@ public class ModItems {
     public static Item pureEnergy;
     public static Item darkEnergy;
     public static Item liteEnergy;
+    public static Item pureEnergySword;
     public static Item pureEnergyPickaxe;
-
+    public static Item pureEnergyAxe;
+    public static Item pureEnergyShovel;
 
     public static void init(){
 
         pureEnergy = new PureEnergy();
         darkEnergy = new DarkEnergy();
         liteEnergy = new LiteEnergy();
+        pureEnergySword = new PureEnergySword();
         pureEnergyPickaxe = new PureEnergyPickaxe();
+        pureEnergyAxe = new PureEnergyAxe();
+        pureEnergyShovel = new PureEnergyShovel();
+
+
+
+
 
     }
 }
---
+++ src/main/java/com/t3ctonic/mysticenergies/items/PureEnergySword.java
@@ -0,0 +1,25 @@
+package com.t3ctonic.mysticenergies.items;
+
+import com.t3ctonic.mysticenergies.creativetab.ModCreativeTabs;
+import com.t3ctonic.mysticenergies.lib.Constants;
+import com.t3ctonic.mysticenergies.material.ModToolMaterials;
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.item.ItemSword;
+
+public class PureEnergySword extends ItemSword {
+
+    private String name = "pureEnergySword";
+
+    public PureEnergySword() {
+
+        super(ModToolMaterials.pureEnergy);
+        this.setUnlocalizedName(Constants.MODID + "_" + name);
+        this.setTextureName(Constants.MODID + ":" + name);
+        GameRegistry.registerItem(this, name);
+        setCreativeTab(ModCreativeTabs.tab);
+
+
+    }
+}
+
+
