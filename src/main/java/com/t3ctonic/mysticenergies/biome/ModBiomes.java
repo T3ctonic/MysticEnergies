8a739135f4fe7b23f0b74da7a68cc755caa59468
T3ctonic <T3ctonicPlays@gmail.com>
2014-10-29 00:59:50 +0000

    Updates

--- src/main/java/com/t3ctonic/mysticenergies/biome/ModBiomes.java
+++ src/main/java/com/t3ctonic/mysticenergies/biome/ModBiomes.java
@@ -22,48 +22,6 @@ public final class ModBiomes {
         //noinspection deprecation
         BiomeManager.warmBiomes.add(new BiomeManager.BiomeEntry(fallenEnd, 10));
 
-        BiomeManager.removeSpawnBiome(BiomeGenBase.beach);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.birchForest);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.birchForestHills);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.coldBeach);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.coldTaiga);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.coldTaigaHills);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.deepOcean);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.desert);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.desertHills);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.extremeHills);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.extremeHillsEdge);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.extremeHillsPlus);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.forest);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.forestHills);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.frozenOcean);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.frozenRiver);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.hell);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.iceMountains);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.icePlains);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.jungle);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.jungleEdge);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.jungleHills);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.megaTaiga);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.megaTaigaHills);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.mesa);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.mesaPlateau);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.mesaPlateau_F);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.mushroomIsland);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.mushroomIslandShore);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.ocean);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.plains);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.river);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.roofedForest);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.savanna);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.savannaPlateau);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.sky);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.stoneBeach);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.swampland);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.taiga);
-        BiomeManager.removeSpawnBiome(BiomeGenBase.taigaHills);
-
-        BiomeManager.addSpawnBiome(fallenEnd);
 
     }
 
